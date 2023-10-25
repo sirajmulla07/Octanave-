61e5e3f719cd0bcb87d0537458eba06233fa0198
sirajmulla07 <131845823+sirajmulla07@users.noreply.github.com>
2023-10-25 15:57:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const taskInput = document.getElementById("taskInput");
-    const addTask = document.getElementById("addTask");
-    const taskList = document.getElementById("taskList");
+// Get references to the DOM elements
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
 
-    addTask.addEventListener("click", function () {
-        const taskText = taskInput.value;
-        if (taskText.trim() !== "") {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `
-                <span>${taskText}</span>
-                <button class="complete">Complete</button>
-                <button class="delete">Delete</button>
-            `;
+// Load tasks from local storage if available
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-            listItem.querySelector(".complete").addEventListener("click", function () {
-                listItem.classList.toggle("completed");
-            });
-
-            listItem.querySelector(".delete").addEventListener("click", function () {
-                listItem.remove();
-            });
-
-            taskList.appendChild(listItem);
-            taskInput.value = "";
-        }
+// Function to render tasks
+function renderTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${task}</span>
+            <button class="delete-button" data-index="${index}">Delete</button>
+        `;
+        taskList.appendChild(li);
     });
+}
+
+// Initial rendering
+renderTasks();
+
+// Function to add a new task
+function addTask() {
+    const newTask = taskInput.value.trim();
+    if (newTask !== "") {
+        tasks.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        taskInput.value = "";
+        renderTasks();
+    }
+}
+
+// Function to delete a task
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+}
+
+// Event listeners
+addTaskButton.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+taskList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        const index = parseInt(event.target.getAttribute("data-index"));
+        deleteTask(index);
+    }
 });
